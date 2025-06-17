34fce0da055bc7de3e78d879105f09b09de6829a
imadi7 <160782414+imadi7@users.noreply.github.com>
2025-06-18 04:43:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ app.include_router(auth_router)
 app.include_router(product_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
