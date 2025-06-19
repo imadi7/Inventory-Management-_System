a420f955b3aebd4d604ba68ee9abd186beef0e25
imadi7 <160782414+imadi7@users.noreply.github.com>
2025-06-19 20:44:45 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,25 +1,14 @@
-from fastapi import APIRouter, HTTPException, status, Depends
-from fastapi_jwt_auth import AuthJWT
-from fastapi_jwt_auth.exceptions import AuthJWTException
-from schemas import UserLogin, TokenResponse
-from database import SessionLocal
-from models import User
-from passlib.hash import bcrypt
-
-router = APIRouter()
-
-@router.post("https://inventory-management-system-l9qq.onrender.com/login", response_model=TokenResponse)
+@router.post("/login", response_model=TokenResponse)
 def login(user: UserLogin, Authorize: AuthJWT = Depends()):
     db = SessionLocal()
-    db_user = db.query(User).filter(User.username == "user.username").first()
+    db_user = db.query(User).filter(User.username == user.username).first()
 
-    if not db_user or not bcrypt.verify(user.password, "db_user.password"):
+    if not db_user or not bcrypt.verify(user.password, db_user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid username or password"
         )
 
-    # Create JWT access token
     access_token = Authorize.create_access_token(subject=user.username)
 
     return {
